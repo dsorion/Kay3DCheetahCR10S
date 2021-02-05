f9f88e8d40a4b2ff98480b95d3f33ed394c8e7c5
dsorion <74698112+dsorion@users.noreply.github.com>
2021-02-05 01:43:30 -0500

    Cheetah working 1.0.4
    Edit probe points

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -235,7 +235,7 @@ Board name: FYSETCV1_1A, FYSETCV1_1B, FYSETCV1_2A, FYSETCV1_2B, change_value = S
 
 #if ENABLED(BED_PROBE_GRID_COUNT)  //You can ignore this line of code
 
-  #define GRID_MAX_POINTS_X 8 //Recommended value: 4. If you want to probe a 3x3 grid (9 times), choose 3. Choose 5 if you want to probe a 5x5 grid (25 times)
+  #define GRID_MAX_POINTS_X 6 //Recommended value: 4. If you want to probe a 3x3 grid (9 times), choose 3. Choose 5 if you want to probe a 5x5 grid (25 times)
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X //You can ignore this line of code if you are unfamiliar with it. 
 #endif
 
